46ef9715388959b16172087fe835ec75c9c817b4
Brian Miller <bmiller@southcomm.com>
2020-02-24 08:34:42 -0600

    add Systems Integrator export

---
+++ tenants/vision-systems/exports/site/www.vision-systems.com/systems-integrator/queries/content.js
@@ -0,0 +1,39 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+query InDesignExportCompaniesByCategory($input: AllPublishedContentQueryInput!) {
+  allPublishedContent(input: $input) {
+    totalCount
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+    edges {
+      cursor
+      node {
+        id
+        name
+        type
+        taxonomy {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+        ... on ContentCompany {
+          state
+          country
+        }
+        primaryImage {
+          id
+          source {
+            name
+          }
+          filePath
+        }
+      }
+    }
+  }
+}
+`;
