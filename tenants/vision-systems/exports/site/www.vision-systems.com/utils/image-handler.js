ac26b9e569daf62a371bdf12853d8f985090c35c
Brian Miller <bmiller@southcomm.com>
2020-02-28 10:00:44 -0600

    fix folder path

--- tenants/vision-systems/exports/site/www.vision-systems.com/utils/image-handler.js
+++ tenants/vision-systems/exports/site/www.vision-systems.com/utils/image-handler.js
@@ -6,7 +6,7 @@ const AWS = require('aws-sdk');
 const {
   AWS_ACCESS_KEY,
   AWS_SECRET_ACCESS_KEY,
-} = require('../../../env');
+} = require('../../../../env');
 
 const s3 = new AWS.S3({
   accessKeyId: AWS_ACCESS_KEY,
