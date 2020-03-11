c09428ff6d975bf66dcdbde6ce5083ca5480a3eb
Brian Miller <bmiller@southcomm.com>
2020-03-11 12:40:44 -0500

    clean up category export for lfw

--- tenants/laserfocusworld/exports/site/www.laserfocusworld.com/categories/index.txt.js
+++ tenants/laserfocusworld/exports/site/www.laserfocusworld.com/categories/index.txt.js
@@ -1,39 +1,14 @@
 const { getAsArray } = require('@base-cms/object-path');
-const paginateQuery = require('@endeavor-business-media/common/paginate-query');
-
 const allPublishedContentQuery = require('./queries/content');
 const websiteSectionsQuery = require('./queries/sections');
 const { downloadImages, zipItUp, uploadToS3 } = require('../utils/image-handler');
+const { retrieveCompanies } = require('../utils/retrieve-companies');
+const { retrieveRootSection } = require('../utils/retrieve-root-section');
+const { retrieveFilterdCompanies } = require('../utils/retrieve-filtered-companies');
 const { formatText } = require('../utils/format-text');
 
 const exportName = `export-${Date.now()}.zip`;
 const companyLogos = [];
-
-const retrieveRootSection = async (client) => {
-  const { data: { websiteSectionAlias } } = await client.query({
-    query: websiteSectionsQuery,
-    variables: { input: { alias: 'directory' } },
-  });
-  return websiteSectionAlias;
-};
-
-const retrieveCompanies = async (apollo) => {
-  const promise = await paginateQuery({
-    client: apollo,
-    query: allPublishedContentQuery,
-    variables: { input: { includeContentTypes: 'Company', pagination: { limit: 250 } } },
-    cursorPath: 'input.pagination.after',
-    rootValue: 'allPublishedContent',
-  });
-
-  const now = Date.now().valueOf();
-  return promise.map((company) => {
-    const sectionIds = getAsArray(company, 'websiteSchedules')
-      .filter(({ start, end }) => start < now && (!end || end > now))
-      .map(({ section }) => section.id);
-    return { ...company, sectionIds };
-  });
-};
 
 const mapHierarchy = (sections, companies) => sections.reduce((arr, section) => {
   const childNodes = getAsArray(section, 'children.edges').map(({ node }) => node);
@@ -50,42 +25,15 @@ const mapHierarchy = (sections, companies) => sections.reduce((arr, section) => 
   ];
 }, []).sort((a, b) => a.fullName.localeCompare(b.fullName));
 
-/**
- * File 1 – Categories:
- * Sort all companies assigned to category alphabetically
- * Companies sorted by sort name field which was not imported into Base in original import
- * Spacing with hidden font stylesheets were added as needed to control output,
- *   keep settings and output in InDesign
- * Not sure how ad cross-reference will work with Base
- *
- * Output:
- * <style=Catsubhead1> Father Category Name <hard return>
- * <style=Catsubhead2> Lowest Category Name <hard return>
- * <style=CatCoName> Company Name <hard return>
- *
- * If statements:
- * If no companies assigned to category, do not output category name
- * If logo assigned to company for that category only, output logo above company name <style=Logo>
- * If display ad assigned to company, output text <style=AdReference> ‘(see ad p ‘ (don’t forget
- *  extra space) <hard return>
- * If logo and display ad assigned, then output both
- * Use new style sheets for companies with ads and logos: <style=CatCoNameLogo>, <style=CatCoNameAd>
- *  , <style=CatCoNameLogoAd>, etc.
- * Entries with ads and logos should have an extra line above and below <style=WhiteSpaceStart> and
- *  <style=WhiteSpaceEnd> to designate the space as “ad space” We would then need to run a script in
- *  InDesign to measure the percentage of page space that listing occupies and calculate the ad-to
- *  -editorial ratio that is a post office requirement and can affect the cost of mailing the issue.
- *
- * Samples
- * 2019_VSD_Cats.pdf = sample PDF of complete InDesign page
- * VSD_Categories.txt = marked up/tagged texted file used for InDesign placement
- * VSD_Categories.xslt = XSLT or template file applied to XML output of data used to produce file
- *
- */
 module.exports = async ({ apollo }) => {
-  const rootSection = await retrieveRootSection(apollo);
+  // This will return the direct decents of the /directory section.
+  const rootSection = await retrieveRootSection(apollo, websiteSectionsQuery, 'directory');
   const sections = getAsArray(rootSection, 'children.edges').map(({ node }) => node);
-  const companies = await retrieveCompanies(apollo);
+  // Get all companies scheduled to the site after Feb. 15 2018
+  // Date is set in retrieveCompanies function
+  const allCompanies = await retrieveCompanies(apollo, allPublishedContentQuery);
+  // Filter companies to only ones scheduled to /directory or below
+  const companies = retrieveFilterdCompanies(allCompanies, rootSection);
 
   // Get the top-level sections and map companies into them
   const segments = await mapHierarchy(sections, companies);
