667c8a0aacd6874fd83e3de664551e217667ed21
Brian Miller <brian@parameter1.com>
2021-07-01 14:18:16 -0500

    Updarte ParaStyles to match old export

--- tenants/minexpo/exports/site/minexpo.ascendmedia.com/alpha-listings/index.txt.js
+++ tenants/minexpo/exports/site/minexpo.ascendmedia.com/alpha-listings/index.txt.js
@@ -10,7 +10,7 @@ module.exports = async ({ apollo }) => {
   companies.sort((a, b) => a.name.localeCompare(b.name));
 
   // Wrap content in paragraph style
-  const printContent = arr => arr.map((c) => {
+  const printContent = arr => arr.map((c, index) => {
     const text = [];
     const companyLetter = c.name.substr(0, 1);
     if (currentLetter !== companyLetter) {
@@ -19,31 +19,41 @@ module.exports = async ({ apollo }) => {
       if (currentLetter.toUpperCase() !== companyLetter.toUpperCase()) {
         if (!companyLetter.match(regex)) {
           currentLetter = companyLetter;
-          text.push(`<ParaStyle:cLetter>${currentLetter}`);
+          text.push(`<ParaStyle:AlphaHead>${currentLetter}`);
         }
       }
     }
-    text.push(`<ParaStyle:cName>${formatText(c.name)}`);
-    if (c.boothNumber) text.push(`<ParaStyle:cBoothNumber>${formatText(c.boothNumber)}`);
-    if (c.address1) text.push(`<ParaStyle:cAddress>${c.address1}`);
-    if (c.address2) text.push(`<ParaStyle:cAddress>${c.address2}`);
-    if (c.cityStateZip && c.country && !['us', 'united states', 'usa'].includes(c.country.toLowerCase())) {
-      text.push(`<ParaStyle:cAddress>${c.cityStateZip} ${c.country}`);
-    } else if (c.cityStateZip) {
-      text.push(`<ParaStyle:cAddress>${c.cityStateZip}`);
+    const accountType = (c.primarImage) ? 'Acct_logo' : 'Acct';
+    if (c.primarImage) text.push(`<ParaStyle:Logo>${c.primaryImage.source.name}`);
+    if (c.boothNumber) {
+      text.push(`<ParaStyle:Co>${formatText(c.name)} \t ${formatText(c.boothNumber)}`);
+    } else {
+      text.push(`<ParaStyle:Co>${formatText(c.name)}`);
     }
-    if (c.phone) text.push(`<ParaStyle:cPhoneNumbers>P ${c.phone}`);
-    if (c.tollfree) text.push(`<ParaStyle:cPhoneNumbers>TFP: ${c.tollfree}`);
-    // if (c.fax) text.push(`<ParaStyle:cPhoneNumbers>Fax: ${c.fax}`);
-    if (c.publicEmail) text.push(`<ParaStyle:cEmail>E ${c.publicEmail}`);
-    if (c.website) text.push(`<ParaStyle:cWebsite>W ${c.website}`);
-    if (c.teaser) text.push(`<ParaStyle:cDescription>${c.teaser}`);
+    if (c.address1) text.push(`<ParaStyle:Add>${formatText(c.address1)}`);
+    if (c.address2) text.push(`<ParaStyle:Add>${formatText(c.address2)}`);
+    if (c.cityStateZip && c.country && !['us', 'united states', 'usa'].includes(c.country.toLowerCase())) {
+      text.push(`<ParaStyle:Add>${formatText(c.cityStateZip)} ${formatText(c.country)}`);
+    } else if (c.cityStateZip) {
+      text.push(`<ParaStyle:Add>${formatText(c.cityStateZip)}`);
+    }
+    if (c.phone) text.push(`<ParaStyle:PhoneWeb>P ${c.phone}`);
+    if (c.tollfree) text.push(`<ParaStyle:PhoneWeb>TFP: ${c.tollfree}`);
+    // if (c.fax) text.push(`<ParaStyle:PhoneWeb>Fax: ${c.fax}`);
+    if (c.website) text.push(`<ParaStyle:PhoneWeb>W ${c.website}`);
+    if (c.teaser && c.teaser !== '...') {
+      text.push(`<ParaStyle:Desc>${c.teaser}`);
+    } else {
+      text.push('<ParaStyle:Desc>Need To Figure Out how this will be populated');
+    }
+    if (c.publicEmail) text.push(`<ParaStyle:Desc>E ${c.publicEmail}`);
+    text.push(`<ParaStyle:${accountType}>${index}`);
     return text.join('\n');
   });
 
   const lines = [
     '<ASCII-MAC>', // @todo detect and/or make query a param
-    '<ParaStyle:cLetter>123',
+    '<ParaStyle:AlphaHead>123',
     ...printContent(companies),
   ];
   const cleanLines = lines.filter(e => e);
