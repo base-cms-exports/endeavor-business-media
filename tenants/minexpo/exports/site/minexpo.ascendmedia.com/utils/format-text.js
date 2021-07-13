0939a5f1835a86c1b8146469f5859597f1398e05
Brian Miller <brian@parameter1.com>
2021-07-13 07:42:24 -0500

    addmore format text values

--- tenants/minexpo/exports/site/minexpo.ascendmedia.com/utils/format-text.js
+++ tenants/minexpo/exports/site/minexpo.ascendmedia.com/utils/format-text.js
@@ -46,11 +46,13 @@ const formatText = (t) => {
   trans.push({ from: '&bull;', to: '<0x2022>' });
   trans.push({ from: '|0x2022|', to: '<0x2022>' });
   trans.push({ from: '&ndash;', to: '<0x2013>' });
+  trans.push({ from: '—', to: '<0x2014>' });
   trans.push({ from: '&mdash;', to: '<0x2014>' });
   trans.push({ from: '&trade;', to: '<0x2122>' });
   trans.push({ from: '&copy;', to: '<0x00A9>' });
   trans.push({ from: '®', to: '<0x00AE>' });
   trans.push({ from: '&reg;', to: '<0x00AE>' });
+  trans.push({ from: 'º', to: '<0x00B0>' });
   trans.push({ from: '&deg;', to: '<0x00B0>' });
   trans.push({ from: '\\', to: '\\\\' });
   // eslint-disable-next-line no-useless-escape
